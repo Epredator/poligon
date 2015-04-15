15ac89487e9c0a976151eb880692cb801df37e44
Adam Trojnar <atrojnar@kontomierz.pl>
2015-04-15 16:06:03 +0200

    added exercise from book: Java head first

--- TUL/helloWorldApp/src/com/company/Main.java
+++ TUL/helloWorldApp/src/com/company/Main.java
@@ -2,10 +2,16 @@ package com.company;
 
 public class Main {
 
+
+
+
+
+
     public static void main(String[] args) {
       for (int i=0;i<5;i++) {
         System.out.println("pętla "+i+1);
       }
       System.out.println("koniec petli");
     }
+
 }
