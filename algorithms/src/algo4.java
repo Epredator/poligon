677007c4336887991dcff1be1df479b94ecfc541
epredator <trojnaradam@gmail.com>
2015-05-28 22:16:14 +0200

    Refactoring kodu algo4 oraz testu

--- algorithms/src/algo4.java
+++ algorithms/src/algo4.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
  * User: trojnaradam@gmail.com
  * Date: 24.05.15
  * Time: 10:02
- * znajdŸ liczbê, która jest najmniejsza i wystêpuje w obydwu podanych tablicach
+ * znajdz liczbe, ktora jest najmniejsza i wysteouje w obydwu tablicach
  *
  * #@param A firts table to compare
  * #@param B second table
@@ -19,14 +19,10 @@ public class algo4 {
       int n = tab1.length;
       int m = tab2.length;
 
-      int i = 0;
-      for (int j = 0; j < n; j++) {
-          for(int z = 0; z < n; z++){
-              if (tab1[j] == tab2[z])
+      for (int j = 0; j < n; j++)
+          for(int i = 0; i < m; i++)
+              if (tab1[j] == tab2[i])
                   return tab1[j];
-
-          }
-      }
     return -1;
   }
 }
