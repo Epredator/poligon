29873f3852fcc6137751d9912125204e2211ac87
epredator <trojnaradam@gmail.com>
2015-07-14 11:22:24 +0400

    algorytm SelectionSort

--- algorithms/test/selectionSortTest.java
+++ algorithms/test/selectionSortTest.java
@@ -1,19 +1,20 @@
 import org.junit.Assert;
 import org.junit.Test;
-import sortAlgo.InsertionSort;
+import sortAlgo.SelectionSort;
 
 /**
- * Created by Adam on 2015-07-13.
+ * Created by Adam on 2015-07-14.
  */
-public class insertionSortTest {
+public class selectionSortTest {
     @Test
-    public void firstTest() throws Exception {
-        InsertionSort a = new InsertionSort();
+    public void firstTest(){
+        SelectionSort a = new SelectionSort();
         int[] tabBeforeSort;
         int[] tabAfterSort;
         tabBeforeSort = new int[]{1, 5, 3, 4, 6, 7};
         tabAfterSort = new int[]{1, 3, 4, 5, 6, 7};
         int[] result = a.sort(tabBeforeSort);
         Assert.assertArrayEquals(tabAfterSort, result);
+
     }
 }
