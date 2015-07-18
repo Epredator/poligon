78a5202d71fdda43d14895e41089dfadff4bd71f
epredator <trojnaradam@gmail.com>
2015-07-18 23:09:20 +0400

    testowanie wyrażen lambda w Java8

---
+++ algorithms/src/functional/bestScoreFinder.java
@@ -0,0 +1,32 @@
+package functional;
+
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
+/**
+ * Created by Adam on 2015-07-17.
+ */
+public class bestScoreFinder {
+//    new ArrayList<Student> students = new ArrayList<Student>();
+//
+//    public void findScore(){
+//        double highestScore = students
+//                .filter(new Predicate<Student>(){
+//                    public boolean op(Student s){
+//                        return s.getGradYear() = 2011;
+//                    }
+//                })
+//                .map(new Mapper<Student, Double>(){
+//
+//                     }
+//
+//
+//                )
+//    }
+
+    private class Student {
+        public boolean getGradYear() {
+            return false;
+        }
+    }
+}
