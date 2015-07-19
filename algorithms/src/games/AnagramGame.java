0d93262aacd7399d663387debb8f2a785727f1dd
epredator <trojnaradam@gmail.com>
2015-07-19 23:30:17 +0400

    anagram algorithm not finish yet!

--- algorithms/src/games/AnagramGame.java
+++ algorithms/src/games/AnagramGame.java
@@ -1,9 +1,17 @@
 package games;
 
+import java.util.Random;
+
 /**
  * Created by Adam on 2015-07-17.
  */
 public class AnagramGame {
+    char[] tempTab = null;
+    char[] sourceWord = null;
+    int sourceSize = 0;
+    int excludeChar = -1;
+
+
     public char[] makeAnagram(char[] letters){
         return null;
     }
@@ -12,4 +20,37 @@ public class AnagramGame {
     public boolean checkQualityOfAnagram(char[] firstAnagram, char[] secondAnagram) {
         return false;
     }
-}
+
+
+    public char[] createAnagram(char[] sourceWord) {
+
+        this.sourceWord = sourceWord;
+        sourceSize = sourceWord.length;
+        tempTab = new char[sourceSize];
+        char[] newWord = new char[sourceSize];
+
+        for(int i=0; i<sourceSize; i++){
+            addCharsToTemp(i);
+            Random r = new Random();
+            int randomCharFromSource = r.nextInt(tempTab.length);
+            excludeChar = randomCharFromSource;
+            newWord[i]=tempTab[randomCharFromSource];
+        }
+
+        return newWord;
+    }
+
+    private void addCharsToTemp(int iterationNumber) {
+        tempTab = new char[sourceSize-iterationNumber];
+
+            int position =0 ;
+        for(int j=0; j<sourceSize; j++){
+            if (j != excludeChar && tempTab.length > position){
+                    tempTab[position] = sourceWord[j];
+                    position++;
+            }
+            }
+        }
+
+
+    }
--- algorithms/test/gameTest/AnagramTest.java
+++ algorithms/test/gameTest/AnagramTest.java
@@ -13,8 +13,9 @@ public class AnagramTest {
     public void checkAnagram(){
         AnagramGame a = new AnagramGame();
         char[] firstAnagram = new char[]{'a','d','a','m'};
-        char[] secondAnagram = new char[]{'d','a' ,'m','a'};
+
 //         int[] result = a.bubbleSort(tabBeforeSort);
+        char[] secondAnagram = a.createAnagram(firstAnagram);
         boolean checker = a.checkQualityOfAnagram(firstAnagram,secondAnagram);
         Assert.assertTrue(checker); //if return true the Anagram is properly
 
