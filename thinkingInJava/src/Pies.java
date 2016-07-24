99169ad7f62324b010a6cdacfd9b5ab90b677085
Adam Trojnar <trojnaradam@gmail.com>
2016-07-24 23:08:27 +0200

    another easy exercise from "Head first Java" book

---
+++ thinkingInJava/src/Pies.java
@@ -0,0 +1,14 @@
+/**
+ * User: trojnaradam@gmail.com
+ * Date: 24.07.16
+ * Time: 23:05
+ */
+public class Pies {
+  int wielkosc;
+  String rasa;
+  String imie;
+
+  void szczekaj(){
+    System.out.println("hauu hauu!");
+  }
+}
---
+++ thinkingInJava/src/PiesTest.java
@@ -0,0 +1,13 @@
+/**
+ * User: trojnaradam@gmail.com
+ * Date: 24.07.16
+ * Time: 23:06
+ */
+public class PiesTest {
+  public static void main(String[] args){
+    //tutaj kod testujący klasę test
+    Pies p = new Pies();
+    p.wielkosc = 40;
+    p.szczekaj();
+  }
+}
