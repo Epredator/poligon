8c26e073e53a59a9c034f9b871e9ee578383756d
epredator <trojnaradam@gmail.com>
2018-02-15 23:46:04 +0100

    added lazy loading sample to improve efficiency

--- thinkingInJava/src/pl/etroya/design/crational/singelton/DbSingelton.java
+++ thinkingInJava/src/pl/etroya/design/crational/singelton/DbSingelton.java
@@ -1,13 +1,16 @@
 package pl.etroya.design.crational.singelton;
 
 public class DbSingelton {
-    private static DbSingelton instance = new DbSingelton();
+    private static DbSingelton instance = null;
 
     private DbSingelton(){
 
     }
 
     public static DbSingelton getInstance() {
+        if(instance.equals(null)){
+            instance = new DbSingelton();
+        }
         return instance;
     }
 }
