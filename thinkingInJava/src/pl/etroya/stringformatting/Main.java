01842696818076a3a2e536c419d45c5804d26c92
epredator <trojnaradam@gmail.com>
2018-03-03 23:21:45 +0100

    string formattig tests

--- thinkingInJava/src/pl/etroya/stringformatting/Main.java
+++ thinkingInJava/src/pl/etroya/stringformatting/Main.java
@@ -13,6 +13,21 @@ public class Main {
 
         String s4 = String.format("The average age between each is %.1f years", avgDiff);
 
+        String s5 = String.format("%d", 32); //out 32
+        String s6 = String.format("%o", 32); //out octal value 40
+        String s7 = String.format("%x", 32); //out hex 20
+        String s8 = String.format("%#o", 32); //out 040 (include zero-padding)
+        String s9 = String.format("%#x", 32); //out 0x20 (include radix)
+        String s10 = String.format("%#X", 32); //out 0X20
+        String printParamsWith4Spaces = String.format("W:%4d X:%4d", 6, 212);
+        String printParamsWith4SpacesAndZeros = String.format("W:%04d X:%04d", 6, 212);
+
+        String printParamsWith4SpacesToLeftSide = String.format("W:%-4d X:%-4d", 6, 212);
+
+        //argument index
+        String s11 = String.format("%d %d %d", 100, 200, 300);
+        String s11 = String.format("%$3d %$1d %$2d", 100, 200, 300); //out 300 100 200
+
         System.out.println(s4);
 
     }
