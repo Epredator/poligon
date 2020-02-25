00ea0501795856c371c5ec6c19c92227b75c0700
Adam <trojnaradam@gmail.com>
2020-02-25 13:13:13 +0100

    example of using a basic Lambda expression and other options how to do it for comparision purpose

--- algorithms/src/functional/LambdaExample.java
+++ algorithms/src/functional/LambdaExample.java
@@ -2,11 +2,10 @@ package functional;
 
 public class LambdaExample {
     public static void main(String[] args) {
-        new Thread(new CodeToRun()).start();
+        new Thread( () -> System.out.println("Printing from the Runnable")).start();
 
     }
 }
-
 
 class CodeToRun implements Runnable {
 
@@ -15,4 +14,58 @@ class CodeToRun implements Runnable {
         System.out.println("Printing the runnable");
     }
 }
+
+//traditional way
+//where is created class CodeToRun.class which is implementing Runnable interface
+//class CodeToRun implements Runnable {
+//
+//    @Override
+//    public void run() {
+//        System.out.println("Printing the runnable");
+//    }
+//}
+//
+//
+//public class LambdaExample {
+//    public static void main(String[] args) {
+//        new Thread(new CodeToRun()).start();
+//
+//    }
+//}
+//
+//
+//class CodeToRun implements Runnable {
+//
+//    @Override
+//    public void run() {
+//        System.out.println("Printing the runnable");
+//    }
+//}
+
+//Second approach with anonymous class
+
+//package functional;
+//
+//public class LambdaExample {
+//    public static void main(String[] args) {
+//        new Thread(new Runnable() {
+//            @Override
+//            public void run() {
+//                System.out.println("printing from the Runnable");
+//            }
+//        }).start();
+//
+//    }
+//}
+//
+//
+//class CodeToRun implements Runnable {
+//
+//    @Override
+//    public void run() {
+//        System.out.println("Printing the runnable");
+//
+//    }
+//}
+
 
