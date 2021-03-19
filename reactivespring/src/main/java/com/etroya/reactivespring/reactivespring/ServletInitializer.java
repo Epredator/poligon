e2ed55c45132d14d010345ec56ea8dd92c2b1226
Adam <trojnaradam@gmail.com>
2021-03-19 23:17:54 +0100

    initial reactive api with sping

---
+++ reactivespring/src/main/java/com/etroya/reactivespring/reactivespring/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.etroya.reactivespring.reactivespring;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ReactivespringApplication.class);
+	}
+
+}
