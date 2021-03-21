433c48265f4629b0f70c4d9075bff68eafcedddd
Adam <trojnaradam@gmail.com>
2021-03-21 13:23:02 +0100

    new endpoint with flux

--- reactivespring/src/test/java/com/etroya/reactivespring/reactivespring/FluxAndMonoTest.java
+++ reactivespring/src/test/java/com/etroya/reactivespring/reactivespring/FluxAndMonoTest.java
@@ -2,19 +2,33 @@ package com.etroya.reactivespring.reactivespring;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
 
     @Test
     void fluxTest() {
-    Flux<String> stringFlux = Flux.just("Spring", "Spring boot", "Reactive Spring")
+        Flux<String> stringFlux = Flux.just("Spring", "Spring boot", "Reactive Spring")
 //            .concatWith(Flux.error(new RuntimeException("Exception occured")))
-            .concatWith(Flux.just("After error"))
-            .log();
-    stringFlux.subscribe(System.out::println,
-            (e) -> System.err.println("Exception is: " + e),
-            ()  -> System.out.println("Completed")
-            );
+                .concatWith(Flux.just("After error"))
+                .log();
+        stringFlux.subscribe(System.out::println,
+                (e) -> System.err.println("Exception is: " + e),
+                () -> System.out.println("Completed")
+        );
+
+    }
+
+    @Test
+    void shouldReturnFluxElementsWithoutError() {
+        Flux<String> stringFlux = Flux.just("Spring", "Spring boot", "Reactive Spring")
+                .log();
+
+        StepVerifier.create(stringFlux)
+                .expectNext("Spring")
+                .expectNext("Spring boot")
+                .expectNext("Reactive Spring")
+                .verifyComplete();
 
     }
 }
