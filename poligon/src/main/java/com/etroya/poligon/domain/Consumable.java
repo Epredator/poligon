d822b528fc4d58a58ce9b77405862f54479eb466
Adam <trojnaradam@gmail.com>
2021-03-28 23:11:05 +0200

    initilized interfaces

---
+++ poligon/src/main/java/com/etroya/poligon/domain/Consumable.java
@@ -0,0 +1,6 @@
+package com.etroya.poligon.domain;
+
+public interface Consumable {
+    int measure();
+    void consume(int quantity);
+}
---
+++ poligon/src/main/java/com/etroya/poligon/domain/Liquid.java
@@ -0,0 +1,9 @@
+package com.etroya.poligon.domain;
+
+public interface Liquid {
+    public default void prepare(){
+//        pour to a cup
+    }
+
+    int measure();
+}
